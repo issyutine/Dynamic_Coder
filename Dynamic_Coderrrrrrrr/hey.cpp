bb5551d39ae1427fb39142ed9e60e3c1a43484db
Z_E_R_0 <140266881+issyutine@users.noreply.github.com>
2023-09-07 15:36:12 +0900

    202302108_이승주

--- Dynamic_Coderrrrrrrr/hey.cpp
+++ Dynamic_Coderrrrrrrr/hey.cpp
@@ -1,9 +1,39 @@
+
 #include <stdio.h>
 
 int main(void) {
 
-	printf("hello\n");
+
+
+	int num;
+
+	scanf_s("%d", &num);
+
+	printf("%d층 피라미드\n", num);
+
+	for (int i = 0; i < num; i++) {
+
+
+
+
+		for (int j = i; j <= num - 1; j++) {
+
+			printf(" ");
+
+		}
+
+		for (int c = 0; c <= (i * 2); c++) {
+
+			printf("*");
+
+		}
+
+		printf("\n");
+
+	}
 
 	return 0;
 
 }
+
+
