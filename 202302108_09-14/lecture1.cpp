06229661fc8198e1c88329e0a43ac4909842dec1
Z_E_R_0 <140266881+issyutine@users.noreply.github.com>
2023-09-14 14:29:41 +0900

    과제1

---
+++ 202302108_09-14/lecture1.cpp
@@ -0,0 +1,16 @@
+//lecture 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	system("title 정수 1개 입력 후 출력");
+
+	int num;
+	printf("정수를 입력하세요. :");
+	scanf_s("%d", &num);
+	printf("키보드로 입력받은 값은 %d 입니다.\n", num);
+
+	return 0;
+
+}
--- Dynamic_Coderrrrrrrr/hey.cpp
+++ Dynamic_Coderrrrrrrr/hey.cpp
@@ -1,4 +1,6 @@
 
+/*  c실습 1일차 과제
+
 #include <stdio.h>
 
 int main(void) {
@@ -36,4 +38,27 @@ int main(void) {
 
 }
 
+*/
 
+
+//Day Two.
+
+
+
+
+//lecture 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	system("title 정수 1개 입력 후 출력");
+
+	int num;
+	printf("정수를 입력하세요. :");
+	scanf_s("%d", &num);
+	printf("키보드로 입력받은 값은 %d 입니다.\n",num);
+
+	return 0;
+
+}
