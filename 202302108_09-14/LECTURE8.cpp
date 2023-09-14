5517cb3a27aae7192d713e81dccd077067a4ce72
Z_E_R_0 <140266881+issyutine@users.noreply.github.com>
2023-09-14 16:07:51 +0900

    lecture8

---
+++ 202302108_09-14/LECTURE8.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int count, hap = 0;
+
+	for (count = 1; count <= 10; count++) {
+
+		hap += count;
+		printf("%02d까지 누적합 출력 : hap = %02d\n", count, hap);
+
+	}
+
+	printf("1+2+........ = %02d\n", hap);
+
+	return 0;
+
+}
