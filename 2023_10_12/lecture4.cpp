64dcd5fa5fe35d5ed7de32fee28c0a2e9979c061
Z_E_R_0 <140266881+issyutine@users.noreply.github.com>
2023-10-12 15:23:05 +0900

    lecture4

--- 2023_10_12/lecture4.cpp
+++ 2023_10_12/lecture4.cpp
@@ -5,9 +5,21 @@ int main(void) {
 	int score[5] = { 87, 92, 89, 98, 78 };
 	int cnt;
 
-	printf("score[5] = { 87, 92, 89, 98, 78 } 배열 요소 출력\n");
+	printf("score[5] = { 87, 92, 89, 98, 78 } 배열 요소 출력\n"); //배열도 포인터임!
 
 	for (cnt = 0; cnt < 5; cnt++)
 		printf("score[%d] = [%d]\n", cnt, score[cnt]);
 	return 0;
 }
+
+/*     int* p_score = score; (배열도 포인터라& 생략가능)
+ 
+ for (int i = 0; i < 3; i++){
+     printf(("%d\n", score[i]);
+	 }
+
+
+	 or
+
+	 int*p_score = &score ㄱㄴ
+*/
