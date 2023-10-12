52e01cd83a9fb4c9a0a3b38228d784753ff5af36
Z_E_R_0 <140266881+issyutine@users.noreply.github.com>
2023-10-12 15:36:18 +0900

    lecture6

---
+++ 2023_10_12/lecture5.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+void swap(int *pa, int*pb) {
+
+	int a_b = *pa;
+	int b_b = *pb;
+
+	* pa = b_b;
+	*pb = a_b;
+
+
+
+
+}
+
+int main(void) {
+
+	int a = 10;
+	int b = 20;
+
+	int* pt1 = &a;
+	int* pt2 = &b;
+	printf("a의 원래값 : %d, b의 원래값: %d", a, b);
+	swap(&a, &b);
+	printf("a의 원래값 : %d, b의 원래값: %d", a, b);
+}
