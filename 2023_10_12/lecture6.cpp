ada702e0ac0a0b4fe067a8f7b5abd3c4d2902fad
Z_E_R_0 <140266881+issyutine@users.noreply.github.com>
2023-10-12 16:58:37 +0900

    lecture7

--- 2023_10_12/lecture6.cpp
+++ 2023_10_12/lecture6.cpp
@@ -1,32 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void swap(int* pa, int* pb)
-{
-    int c = *pa;
-    pa = pb;
-    pb = c;
+void swap(int* pa, int* pb) {
+	int c = *pa;
+	*pa = *pb;
+	*pb = c;
 }
 
-void sort(int arr)
-{
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = i; j < 5; j++)
-        {
-            if (arr[i] > arr[j])
-                swap(&arr[i], &arr[j]);
-        }
-    }
+void sort(int* arr) {
+
+	for (int i = 0; i < 5; i++) {
+
+		for (int j = i; j < 5; j++) {
+
+			if (arr[i] > arr[j]) {
+				swap(&arr[i], &arr[j]);
+
+			}
+
+		}
+	}
+
 }
 
-int main()
-{
-    int arr[5] = { 5, 4, 1, 2, 3 };
-    printf("arr = %d, %d, %d, %d, %d\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+int main() {
 
-    sort(&arr[0]);
+	int arr[5] = { 5, 1, 4, 3, 2 };
+	printf("arr = %d %d %d %d %d\n", arr[0], arr[1], arr[2], arr[3], arr[4]);
+	sort(&arr[0]);
+	printf("reverce arr = %d, %d, %d, %d, %d", arr[0], arr[1], arr[2], arr[3], arr[4]);
 
-    printf("reverce arr = %d, %d, %d, %d, %d", arr[0], arr[1], arr[2], arr[3], arr[4]);
-
-    return 0;
+	return 0;
 }
